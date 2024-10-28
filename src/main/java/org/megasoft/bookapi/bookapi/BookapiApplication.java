82b28d14b59832bed2d11a96575dfd4328b41f92
davidramireza <44451410+davidramireza@users.noreply.github.com>
2024-10-28 06:02:08 -0500

    first commit
    este commit trae toda una api con spring boot , donde genera un CRUD almacena con h2

---
+++ src/main/java/org/megasoft/bookapi/bookapi/BookapiApplication.java
@@ -0,0 +1,13 @@
+package org.megasoft.bookapi.bookapi;
+
+import org.springframework.*;
+import org.springframework.boot.*;
+
+@SpringBootApplication
+public class BookapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/megasoft/bookapi/bookapi/BookapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.megasoft.bookapi.bookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
